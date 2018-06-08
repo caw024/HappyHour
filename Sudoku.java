24887064a6fc8b2ab0afd4e222038e6982c9e0fb
KendrickLiang <kliang5@stuy.edu>
2018-06-07 20:01:14 -0400

    edit

--- Sudoku.java
+++ Sudoku.java
@@ -1,29 +1,21 @@
-import java.util.ArrayList;
-
 public class Sudoku{
     
-    private Square[][] s;
+    private Cell[][] s;
     
     public Sudoku(){
-	s = new Square[3][3];
+	s = new Cell[9][9];
+	for (int a = 0; a < 18; a++){
+	    s[(int)(Math.random() * 9)][(int)(Math.random() * 9)] = new Cell((int)(Math.random() * 8) + 1);
+	}
     }
     
     public boolean checkRow(int r){
 	int[] row = new int[9];
-	int[] row2 = new int[3];
-	int a = 0;
-	for (int c = 0; c < 3; c++){
-	    row2 = s[r][c].getRow(r);
-	    for (int i: row2){
-		if (i == 0)
-		    return false;
-		row[a] = i;
-		a++;
-	    }
-	}
+	for (int c = 0; c < 9; c++)
+	    row[c] = s[r][c].getValue();
 	for (int x = 0; x < 8; x++){
 	    for (int y = x; y < row.length; y++){
-		if (row[x] == row[y])
+		if (row[x] != 0 && row[y] != 0 && row[x] == row[y])
 		    return false;
 	    }
 	}
@@ -32,20 +24,11 @@ public class Sudoku{
     
     public boolean checkCol(int c){
 	int[] col = new int[9];
-	int[] col2 = new int[3];
-	int a = 0;
-	for (int r = 0; r < 3; r++){
-	    col2 = s[r][c].getCol(c);
-	    for (int i: col2){
-		if (i == 0)
-		    return false;
-		col[a] = i;
-		a++;
-	    }
-	}
+	for (int r = 0; r < 9; r++)
+	    col[r] = s[r][c].getValue();
 	for (int x = 0; x < 8; x++){
 	    for (int y = x; y < col.length; y++){
-		if (col[x] == col[y])
+		if (col[x] != 0 && col[y] != 0 && col[x] == col[y])
 		    return false;
 	    }
 	}
@@ -53,82 +36,140 @@ public class Sudoku{
     }
     
     public boolean checkSquare(int r, int c){
-	return s[r][c].checkSquare();
-    }
-
-    public ArrayList<Integer> ridShare(int[] keep, ArrayList<integer> edit){
-	for (int i = 0; i < keep.length; i++){
-	    for (int k = 0; k < edit.size(); k++){
-		if (keep[i] == edit.get(k))
-			edit.set(k, 0);
+	int[] sqr = new int[9];
+	if (r >= 0 && r <= 2 && c >= 0 && c <= 2){
+	    int i = 0;
+	    for (int x = 0; x < 3; x++){
+		for (int y = 0; y < 3; y++){
+		    sqr[i] = s[x][y].getValue();
+		    i++;
+		}
 	    }
 	}
-	    	
-	for (int i = edit.size() - 1; i >= 0; i--){
-	    if (edit.get(i) == 0)
-		edit.remove(i);
+	if (r >= 3 && r <= 5 && c >= 0 && c <= 2){
+	    int i = 0;
+	    for (int x = 3; x < 6; x++){
+		for (int y = 0; y < 3; y++){
+		    sqr[i] = s[x][y].getValue();
+		    i++;
+		}
+	    }
 	}
-	return edit;
-    }
-
-    public ArrayList<Integer> legalMove(int r, int c){
-	ArrayList<Integer> poss = new ArrayList<Integer>();
-	poss.add(1);
-	poss.add(2);
-	poss.add(3);
-	poss.add(4);
-	poss.add(5);
-	poss.add(6);
-	poss.add(7);
-	poss.add(8);
-	poss.add(9);
-
-	poss = ridShare(s.getRow(r), poss);
-	poss = ridShare(s.getColumn(c), poss);
-	poss = ridShare(s[r][c].getSquare(), poss);
-
-        return poss;
-    }
-    
-    public boolean isFilled(){
-	for (int r = 0; r < 3; r++){
-	    for (int c = 0; c < 3; c++){
-		if(checkCol(c) == false)
-		    return false;
-		if (checkSquare(r, c) == false)
+	if (r >= 6 && r <= 8 && c >= 0 && c <= 2){
+	    int i = 0;
+	    for (int x = 6; x < 9; x++){
+		for (int y = 0; y < 3; y++){
+		    sqr[i] = s[x][y].getValue();
+		    i++;
+		}
+	    }
+	}
+	if (r >= 0 && r <= 2 && c >= 3 && c <= 5){
+	    int i = 0;
+	    for (int x = 0; x < 3; x++){
+		for (int y = 3; y < 6; y++){
+		    sqr[i] = s[x][y].getValue();
+		    i++;
+		}
+	    }
+	}
+	if (r >= 3 && r <= 5 && c >= 3 && c <= 5){
+	    int i = 0;
+	    for (int x = 3; x < 6; x++){
+		for (int y = 3; y < 6; y++){
+		    sqr[i] = s[x][y].getValue();
+		    i++;
+		}
+	    }
+	}
+	if (r >= 6 && r <= 8 && c >= 3 && c <= 5){
+	    int i = 0;
+	    for (int x = 6; x < 9; x++){
+		for (int y = 3; y < 6; y++){
+		    sqr[i] = s[x][y].getValue();
+		    i++;
+		}
+	    }
+	}
+	if (r >= 0 && r <= 2 && c >= 6 && c <= 8){
+	    int i = 0;
+	    for (int x = 0; x < 3; x++){
+		for (int y = 6; y < 9; y++){
+		    sqr[i] = s[x][y].getValue();
+		    i++;
+		}
+	    }
+	}
+	if (r >= 3 && r <= 5 && c >= 6 && c <= 8){
+	    int i = 0;
+	    for (int x = 3; x < 6; x++){
+		for (int y = 6; y < 9; y++){
+		    sqr[i] = s[x][y].getValue();
+		    i++;
+		}
+	    }
+	}
+	if (r >= 6 && r <= 8 && c >= 6 && c <= 8){
+	    int i = 0;
+	    for (int x = 6; x < 9; x++){
+		for (int y = 6; y < 9; y++){
+		    sqr[i] = s[x][y].getValue();
+		    i++;
+		}
+	    }
+	}
+	for (int j = 0; j < 8; j++){
+	    for (int k = j; k < sqr.length; k++){
+		if (sqr[j] != 0 && sqr[k] != 0 && sqr[j] == sqr[k])
 		    return false;
 	    }
-	    if (checkRow(r) == false)
-		return false;
 	}
 	return true;
     }
     
-    public void setOriginal(){
-	for (Square[] a: s){
-	    for (Square b: a){
-		b.setOriginal();
+    public boolean isFilled(){
+	for (int r = 0; r < 9; r++){
+	    for (int c = 0; c < 9; c++){
+		if (s[r][c].getValue() == 0)
+		    return false;
 	    }
 	}
+	return true;
+    }
+        
+    public boolean solve(){
+	return solveH(0);
+    }
+
+    private boolean solveH(int c){
+	for (int r = 0; r < s.length; r++){
+	    addValue(r,c);
+	    if ( c == s.length-1 ) { 
+		return true;
+	    }
+	    else if ( solveH(c + 1) ) {
+		return true;
+	    }
+	    else { 
+		removeValue(r,c);
+	    }
+	}
+	return false;
     }
     
-    public boolean solve(){
-	ArrayList<Integer> poss = new ArrayList<Integer>();
-	for (int r = 0; r < 3; r++){
-	    for (int c = 0; c < 3; c++){
-		poss = legalMove(r,c);
-		if (poss.size() == 0){
-		    //remove last piece
-		}
-		else{
-		    
-		}
-		else{
-		    
-		}
-       
-	    }
-	}
-	
+    private boolean addValue( int r, int c){
+	if (s[r][c].originalValue())
+	    return false;
+	while ((!checkRow(r) || !checkCol(c) || !checkSquare(r, c)) && s[r][c].getValue() < 9)
+	    s[r][c].setValue(s[r][c].getValue() + 1);
+	if (checkRow(r) && checkCol(c) && checkSquare(r, c))
+	    return true;
+    }
+    
+    private boolean removeValue(int r, int c){
+	if (s[r][c].getValue() == 0)
+	    return false;
+	s[r][c].setValue(0);
+	return true;
     }
 }
